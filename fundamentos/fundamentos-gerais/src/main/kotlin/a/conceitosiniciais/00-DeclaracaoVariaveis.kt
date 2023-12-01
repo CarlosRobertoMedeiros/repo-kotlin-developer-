58b041d7e0b1682dbba064753fa0cc15c9da3b3a
Carlos Roberto <carlosmedeiroslima@gmail.com>
2023-12-01 00:56:17 -0300

    Atualização dos fundamentos

--- fundamentos/fundamentos-gerais/src/main/kotlin/a/conceitosiniciais/00-DeclaracaoVariaveis.kt
+++ fundamentos/fundamentos-gerais/src/main/kotlin/a/conceitosiniciais/00-DeclaracaoVariaveis.kt
@@ -1,14 +1,16 @@
+package a.conceitosiniciais
+
 /**
- Type    Bit
- Double  64
- Float   32
- Long    64
- Int     32
- Short   16
- Byte     8
- Boolean  ? 0 u 1 um bit
- String   ?
- Char     ?
+Type    Bit
+Double  64
+Float   32
+Long    64
+Int     32
+Short   16
+Byte     8
+Boolean  ? 0 u 1 um bit
+String   ?
+Char     ?
  */
 fun main() {
     println("Declaração de Variáveis")
@@ -23,13 +25,28 @@ fun main() {
 //    println("String MAX ${String.MAX_VALUE} - Min ${Double.MIN_VALUE}");
     println("Char MAX ${Char.MAX_VALUE} - Min ${Char.MIN_VALUE}");
 }
-fun inicializacaoDeVariaveis(){
-    var c: Char='A'
-    var s:String = "Carlos Roberto"
-    var b:Boolean = false
+
+fun inicializacaoDeVariaveis() {
+
+    var c: Char = 'A'
+    var s: String = "Carlos Roberto"
+    var b: Boolean = false
+
+    var double: Double = 10.0
+    var float: Float = 10f
+    var long: Long = 10L
+    var int: Int = 10
+    var short: Short = 10
+    var byte: Byte = 10
+
+    var idade = 40  // Caso o Kotlin faça a inferência de valor ele entende que a variável é inteira
+    var idade2 : Byte = 40
+    println(idade2);
+
 
 }
-fun numericos(){
+
+fun numericos() {
     println("Double MAX ${Double.MAX_VALUE} - Min ${Double.MIN_VALUE}");
     println("Float MAX ${Float.MAX_VALUE} - Min ${Float.MIN_VALUE}");
     println("Long MAX ${Long.MAX_VALUE} - Min ${Long.MIN_VALUE}");
