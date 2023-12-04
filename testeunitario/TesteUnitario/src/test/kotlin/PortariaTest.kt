0cc347f56295da0023696d0c0808a68b2916ae42
Carlos Roberto <carlosmedeiroslima@gmail.com>
2023-12-03 22:15:54 -0300

    Implementando exemplos de teste unitário

---
+++ testeunitario/TesteUnitario/src/test/kotlin/PortariaTest.kt
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class PortariaTest{
+
+    @Test
+    @DisplayName("Testa os cenarios da portaria")
+    fun testaPortaria(){
+
+    }
+
+}
+
+
